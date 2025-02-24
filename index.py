571b7496b4c946acb29148dfda5c4b319f7fbc01
roshasjohnson <roshasj10@gmail.com>
2025-02-24 10:09:55 +0530

    extracted the data from csv files

--- index.py
+++ index.py
@@ -1,3 +1,18 @@
 import pandas as pd
+import sqlite3
 
-print("Hello world")
+db_connection = sqlite3.connect('data.db')
+cursor = db_connection.cursor()
+
+
+def extract_data():
+    df_a = pd.read_csv("csv_files/order_region_a.csv")
+    df_b = pd.read_csv("csv_files/order_region_b.csv")
+    print(df_a.head())
+    print(df_b.head())
+
+
+def transform_data():
+    pass
+    
+extract_data()
